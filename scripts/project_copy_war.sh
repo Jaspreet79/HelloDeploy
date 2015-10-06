5940768f1cdd80e4cb1d96ba751e93be5fe2e735
Jaspreet79 <jaspreet79@gmail.com>
2015-10-07 01:05:45 +0530

    Update project_copy_war.sh
    script fixed

--- scripts/project_copy_war.sh
+++ scripts/project_copy_war.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo cp  /home/ubuntu/hellodeploy/HelloDeploy/target/CounterWebApp.war /var/lib/tomcat7/webapps/
+sudo cp  /home/ubuntu/deploy/target/CounterWebApp.war /var/lib/tomcat7/webapps/
